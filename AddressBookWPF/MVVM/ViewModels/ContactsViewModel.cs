be2ee6c6ba91bd7f91c963c84579624068ba2b57
Malin-Eriksson <sangsta.malin@gmail.com>
2023-02-02 14:10:06 +0100

    wpf with dependency injection

--- AddressBookWPF/MVVM/ViewModels/ContactsViewModel.cs
+++ AddressBookWPF/MVVM/ViewModels/ContactsViewModel.cs
@@ -25,7 +25,7 @@ namespace AddressBookWPF.MVVM.ViewModels
 
         }
 
-
+/*
         private ContactModel selectedContact = null!;
 
 
@@ -48,7 +48,7 @@ namespace AddressBookWPF.MVVM.ViewModels
             else { }
 
         }
-
+*/
 
 
 
